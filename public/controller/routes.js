8566584eff15a6b5a55aad0a24399619e8491099
borracho-mejor <brandon@borracho.cash>
2022-03-11 20:23:55 -0600

    forgot to remove some sBCH references

--- public/controller/routes.js
+++ public/controller/routes.js
@@ -1,20 +1,17 @@
 import * as HomePage from "../viewpage/home_page.js";
 import * as AboutPage from "../viewpage/about_page.js";
 import * as WhitepaperPage from "../viewpage/whitepaper_page.js";
-import * as SBCHPage from "../viewpage/smartBCH_page.js";
 
 export const routePathname = {
   HOME: "/",
   ABOUT: "/about",
   WHITEPAPER: "/whitepaper",
-  SBCH: "/smartBCH",
 };
 
 export const routes = [
   { pathname: routePathname.HOME, page: HomePage.home_page },
   { pathname: routePathname.ABOUT, page: AboutPage.about_page },
   { pathname: routePathname.WHITEPAPER, page: WhitepaperPage.whitepaper_page },
-  { pathname: routePathname.SBCH, page: WhitepaperPage.whitepaper_page },
 ];
 
 export function routing(pathname, href) {
