513b41f95d18a3fb5663c77ba14e6447c7d8a8ff
borracho-mejor <brandon@borracho.cash>
2022-05-06 20:40:27 -0500

    removed no audit badge

--- public/viewpage/smartBCH_page.js
+++ public/viewpage/smartBCH_page.js
@@ -208,7 +208,7 @@ function buildProjectCard(project, index) {
   }
   let auditTag = "";
   if (Object.keys(project.audit).length === 0) {
-    auditTag += `<div class="inline padding-left"><span class="badge badge-warning">No Audit</span></div>`;
+    // auditTag += `<div class="inline padding-left"><span class="badge badge-warning">No Audit</span></div>`;
   } else {
     for (var key in project.audit) {
       if (project.audit.hasOwnProperty(key)) {
@@ -277,7 +277,7 @@ function buildProjectCard(project, index) {
             <div class="card-header" style="padding: 10px;">
               <div class="inline"><a href=${
                 project.site
-              } target="_blank"><h4 class="inline padding-left-medium ignore-hyper-color" style="vertical-align: middle;">${
+              } target="_blank"><h4 class="inline padding-left-medium padding-right ignore-hyper-color" style="vertical-align: middle;">${
     project.name
   }</h4></a><h6 class="inline" style="vertical-align: middle;">${listingTag}${auditTag}${dyorTag}</h6></div>
               <div class="inline padding-right-medium padding-top-medium float-right"><h6 class="text-muted">${typesText}</h6></div>
