87865858cbfd384bd7d91460b455cdc89f0b7fdd
borracho-mejor <brandon@borracho.cash>
2022-08-26 18:13:09 -0500

    update conflex warning

--- public/viewpage/smartBCH_page.js
+++ public/viewpage/smartBCH_page.js
@@ -141,7 +141,7 @@ export async function build_smartBCH_page(
                           <i class="material-icons-outlined" style="font-size: 1.5em; display: inline-block; justify-content: center; align-items: center;">warning</i>
                         </div>
                         <div>
-                          Most high liquidty bridges and on/off ramps are currently offline due to the recent <a style="color: white;" href="https://borracho.cash/smartbch#project=conflex%20f.k.a.%20coinflex"><strong>Conflex drama</strong></a>, please DYOR before investing.
+                          <a style="color: white;" href="https://borracho.cash/smartbch#project=conflex%20f.k.a.%20coinflex"><strong>Mark Lamb and Conflex</strong></a> really fucked us. Most high liquidty bridges and on/off ramps are currently offline, please DYOR before investing.
                         </div>
                         <div class="px-3 flex-container" style="flex: 10%; text-align: center; vertical-align: middle; flex-direction: column; justify-content: center;">
                           <i class="material-icons-outlined" style="font-size: 1.5em; display: inline-block; justify-content: center; align-items: center;">warning</i>
